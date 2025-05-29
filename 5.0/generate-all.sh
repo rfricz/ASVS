76934c6e7c78c2f8ed73d0e217167ccc318bae73
Richard Fritsch <36163339+rfricz@users.noreply.github.com>
2025-05-29 17:35:52 +0200

    Generate ReqView template for ASVS 5.0

--- 5.0/generate-all.sh
+++ 5.0/generate-all.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 ALLOWED_LANGS=$(tr '\n' ' ' < languages.txt | sed 's/ $//')
 
@@ -24,6 +24,8 @@ for lang in ${LANGS}; do
     python3 tools/export.py --format cdx_json --language $lang > "$verslong.cdx.json"
     python3 tools/export.py --format json_legacy --language $lang > "$verslong.legacy.json"
     python3 tools/export.py --format json --language $lang --verify-only
+
+    python3 tools/export.py --format reqw_json --language $lang > "$verslong.reqw"
 
     python3 tools/export.py --format json_flat --language $lang > "$verslong.flat.json"
     python3 tools/export.py --format json_flat_legacy --language $lang > "$verslong.flat.legacy.json"
